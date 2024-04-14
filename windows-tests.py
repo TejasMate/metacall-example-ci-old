ca8be72b318924fcbb9d30e2bbcc215b8f265153
Tejas Mate <hi.tejas@pm.me>
2024-04-14 08:49:45 +0000

    Updated

--- windows-tests.py
+++ windows-tests.py
@@ -5,10 +5,9 @@ from pathlib import Path
 import subprocess
 
 def run_script(script_file):
-  os.chmod(script_file, 0o755)  # Set execute permissions
-  process = subprocess.Popen([script_file], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-  output, _ = process.communicate()
-  return output.decode().strip()
+    process = subprocess.Popen(["expect", script_file], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    output, _ = process.communicate()
+    return output.decode().strip()
 
 # Get the directory containing the YAML files
 yaml_dir = "examples-testing/test-suits"
@@ -17,6 +16,7 @@ yaml_dir = "examples-testing/test-suits"
 for filename in os.listdir(yaml_dir):
     if filename.endswith(".yaml"):
         yaml_path = os.path.join(yaml_dir, filename)
+
         # Load the YAML file
         with open(yaml_path, "r") as f:
             config = yaml.safe_load(f)
@@ -29,7 +29,7 @@ for filename in os.listdir(yaml_dir):
         project_dir = Path("test-scripts") / project_name
         project_dir.mkdir(parents=True, exist_ok=True)
 
-        # Create script.exp files for each test case
+        # Create script.lua files for each test case
         script_count = 1
         for code_file in code_files:
             file_name = code_file["name"]
@@ -44,32 +44,27 @@ for filename in os.listdir(yaml_dir):
                 command = test_case["command"]
                 command = re.sub(r'"', r'\\"', command)  # Escape all double quotes with backslash
 
-                # Create the script.exp file in the project directory
-                script_file = project_dir.joinpath(f"test-case{script_count}.exp")
+                # Create the script.lua file in the project directory
+                script_file = project_dir.joinpath(f"test-case{script_count}.lua")
                 print(script_file)
                 with open(str(script_file), "w") as f:
-                    f.write("#!/usr/bin/expect -f\n")
-                    f.write("set timeout 1\n")
-                    f.write("spawn metacall\n")
-                    f.write('expect "Welcome to Tijuana, tequila, sexo & marijuana. λ"\n')
-                    f.write(f'send "load {file_extension} {file_path}\\r"\n')
-                    f.write('expect "0\nλ"\n')
-                    f.write(f'send "{command}\\r"\n')
-                    f.write('expect "\\[.*\\]\nλ"\n')
-                    f.write('send "exit\\r"\n')
-                    f.write('expect "Exiting ..."\n')
-                    f.write("expect eof\n")
-
+                    f.write('spawn("metacall")\n')
+                    f.write(f'sendln("load {file_extension} {file_path}")\n')
+                    f.write(f'sendln("{command}")\n')
+                    f.write(f'sendln("exit")\n')
                 main_output = run_script(script_file)
-                output = main_output.replace(" ","")
-                strings_to_replace = ['3m', '9m', '[32m', '[39m', '[1G[0J', '[3G', '[3']
+
+                output = main_output.replace(" ", "")
+                strings_to_replace = ['3m', '9m', '[32m', '[39m', '[1G[0J', '[3G', '[3']
                 for string in strings_to_replace:
                     output = output.replace(string, '')
 
                 expout = test_case["expected-stdout"]
-                expout = expout.replace("\\", "").replace('"', "'")
+                expout = expout.replace("\\", "")
 
-                if expout in output: print(f"Test Case {script_count} has passed")
-                else: raise Exception(f"Test Case {script_count} hasn't passed. \nWarning: {main_output}")
+                if expout in output:
+                    print(f"Test Case {script_count} has passed")
+                else:
+                    raise Exception(f"Test Case {script_count} hasn't passed. \nWarning: {main_output}")
 
                 script_count += 1
